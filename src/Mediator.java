1f88af1f6ee0a6b8a580c63c1359acc247c7215c
imadhou <imadhou00@gmail.com>
2022-04-24 04:34:31 +0200

    sparql

--- src/Mediator.java
+++ src/Mediator.java
@@ -6,11 +6,11 @@ public class Mediator {
     public static void main(String args[]) throws Exception {
 
         RestClient restClient = new RestClient();
+
         JdbcClient jdbcClient = new JdbcClient();
+
         SparqlClient sparqlClient = new SparqlClient();
-        //System.out.println(jdbcClient.getFilmsByTitle("Manito"));
-        //System.out.println(restClient.getDescriptionByTitle("Manito"));
-        //System.out.println(sparqlClient.getDescriptionByTitle("Manito"));
-        System.out.println(sparqlClient.getDescriptionByActor("Fulanito"));
+        sparqlClient.getFilmByTitle("Pirates of the Caribbean");
+        sparqlClient.getFilmsByActor("Kevin McNally");
     }
 }
